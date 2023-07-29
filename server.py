5bca0343c092e4c226ea8acda3126af88c6eb204
Vaibhavtayade20 <vaibhavtayade786@gmail.com>
2023-07-29 10:58:20 +0530

    change

--- server.py
+++ server.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def root():
-    return "welcome to ITIL exam"
+    return "welcome to ITIL exam v1"
 
 
 @app.route("/me", methods=["GET"])
